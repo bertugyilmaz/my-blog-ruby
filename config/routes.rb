0e7a71bd63008aba701cbd3d2a6082e7fcb697fe
Bertuğ Yılmaz <ylmazbertug@gmail.com>
2018-02-21 11:43:07 +0300

    Removed partial post because something went wrong

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  get 'blog/index'
+  resources :blog
+
   get 'home/index'
   root 'home#index'
 end
