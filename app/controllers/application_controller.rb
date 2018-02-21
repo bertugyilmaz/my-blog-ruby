3908f794ec6bfa6372cb0b724592fe87b7d03411
Bertuğ Yılmaz <ylmazbertug@gmail.com>
2018-02-21 11:55:05 +0300

    When the blog#show action was run, the recent post crashed on footer. It was dissolved.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :load_recent_post
   protect_from_forgery with: :exception
+
+  def load_recent_post
+    @recent_posts = Article.order("created_at DESC").limit(4)
+  end
 end
